4af7ae2a8b25cf96ef8ed922c5d520e784743f5d
grifting-goat <137014275+grifting-goat@users.noreply.github.com>
2025-06-19 10:53:08 -0400

    initial commit

--- main.ts
+++ main.ts
@@ -1,3 +1,28 @@
-basic.forever(function () {
-	
-})
+/**
+ * 
+ */
+
+//% color="#1274bd"
+
+namespace NSE {
+
+    /**SENSORS**/
+
+    //PRIVATE
+
+    //PUBLIC - blocks which get exported and used by students
+
+    /**
+     * This should be placed in the start up section
+     */
+    //% block="start with number"
+    //% weight=90
+    //% group="Sensors"
+    //% inlineInputMode=inline
+    export function start(integer: number) {
+
+
+    }
+
+
+}
