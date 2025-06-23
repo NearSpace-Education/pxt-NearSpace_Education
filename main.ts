464c3f887a10d87c10574ed466abf8a3cf4849f0
grifting-goat <137014275+grifting-goat@users.noreply.github.com>
2025-06-23 17:16:25 -0400

    integrated log and lnto lib

--- main.ts
+++ main.ts
@@ -1,9 +1,8 @@
 /**
- * 
+ * main file which provides simplifed ui for students
  */
 
 //% color="#1274bd"
-
 namespace NSE {
 
     /**SENSORS**/
@@ -15,7 +14,7 @@ namespace NSE {
     /**
      * This should be placed in the start up section
      */
-    //% block="start with number"
+    //% block="start with number $integer"
     //% weight=90
     //% group="Sensors"
     //% inlineInputMode=inline
@@ -24,5 +23,39 @@ namespace NSE {
 
     }
 
+    //% color="#8f2929"
+    /**
+     * Returns the logarithm of a number with a specified base.
+     * Returns NaN if base ≤ 0, base = 1, or number ≤ 0.
+     * 
+     * @param base the base of the logarithm (e.g. 2, 10)
+     * @param num the number to take the logarithm of
+     */
+    //% block="log base $base of $num"
+    //% weight=80
+    //% group="Math"
+    //% inlineInputMode=inline
+    export function logarithm(base: number, num: number): number {
+        if (base <= 0 || base == 1 || num <= 0) {
+            return NaN
+        }
+        return Math.log(num) / Math.log(base)
+    }
 
+    /**
+     * Returns the natural logarithm of a number
+     * Returns NaN if number ≤ 0.
+     * @param num the number to take the logarithm of
+     */
+    //% block="ln $num"
+    //% group="Math"
+    //% weight=80
+    //% inlineInputMode=inline
+    export function ln(num: number): number {
+        if (num <= 0) {
+            return NaN
+        }
+        return Math.log(num)
+    }
 }
+
